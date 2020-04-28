b5192afdaff8bf3e0406897d6a97c418787d5640
tayyab203 <61288485+tayyab203@users.noreply.github.com>
2020-04-28 01:35:36 -0700

    commit 7

--- script.js
+++ script.js
@@ -40,4 +40,49 @@ function searchMeal(e) {
             .join('');
         }
       });
-      
+          // Clear search text
+    search.value = '';
+} else {
+  alert('Please enter a search term');
+}
+}
+
+// Fetch meal by ID
+function getMealById(mealID) {
+fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+  .then(res => res.json())
+  .then(data => {
+    const meal = data.meals[0];
+
+    addMealToDOM(meal);
+  });
+}
+
+// Fetch random meal from API
+function getRandomMeal() {
+// Clear meals and heading
+mealsEl.innerHTML = '';
+resultHeading.innerHTML = '';
+
+fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+  .then(res => res.json())
+  .then(data => {
+    const meal = data.meals[0];
+
+    addMealToDOM(meal);
+  });
+}
+
+// Add meal to DOM
+function addMealToDOM(meal) {
+const ingredients = [];
+
+for (let i = 1; i <= 20; i++) {
+  if (meal[`strIngredient${i}`]) {
+    ingredients.push(
+      `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+    );
+  } else {
+    break;
+  }
+}
